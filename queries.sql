66a0b75304658764603a2edff6fd96df9cf7b138
SydneyTK <sydney9797@gmail.com>
2021-12-18 13:16:38 -0500

    created table outlining eligible retiries

---
+++ queries.sql
@@ -0,0 +1,20 @@
+--7.3.1 retierment eligibility 
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31'); 
+
+--7.3.1 number of employees retiring 
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31'); 
+
+--7.3.1 create a new table 
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31'); 
+
+SELECT * FROM retirement_info;
